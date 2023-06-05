fab544d532ae0d9b64a23a9bb083fa931f76186b
AgioStephen <86098063+AgioStephen@users.noreply.github.com>
2023-06-05 10:56:04 +0100

    SC

--- invoke-scuba.ps1
+++ invoke-scuba.ps1
@@ -1,2 +1,12 @@
-﻿# Contents of invoke-scuba-wrapper.ps1
+﻿
+# Define the function to import the module and run the import-scuba command
+function invoke-scuba-gear {
+    Import-Module -Name .\PowerShell\ScubaGear
+    import-scuba
+}
+
+
+invoke-scuba-gear
+
+# Contents of invoke-scuba-wrapper.ps1
 invoke-scuba
